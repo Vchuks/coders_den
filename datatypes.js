a286565d0ba716bb57797d30e86f1195d1bad2d1
Vchuks <64593598+Vchuks@users.noreply.github.com>
2022-04-18 10:47:54 +0100

    assignment 1

---
+++ datatypes.js
@@ -0,0 +1,49 @@
+//question5: Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+
+let var1 = "Onyinye";
+let var2 = 4;
+let var3 = true;
+let var4 = null;
+let var5;
+let var6 = {name:"Peter", age: 6};
+let var7 = ['Ada','John', 'Stephen', 'Abuja'];
+
+console.log(typeof var1);
+console.log(typeof var2);
+console.log(typeof var3);
+console.log(typeof var4);
+console.log(typeof var5);
+console.log(typeof var6);
+console.log(typeof var7);
+
+//question6: Declare four variables without assigning values
+let name;
+let age1;
+let location;
+let color;
+console.log(name,age1,location,color);
+
+//question7: Declare four variables with assigned values
+let people ="congregation";
+let number = 67;
+let location1 = "Abuja";
+let fName = "Onyinye";
+console.log(people,number,location1,fName);
+
+//question8: Declare variables to store your first name, last name, marital status, country and age in multiple lines
+let firstName = "Onyinye";
+let lastName = "Vivian";
+let maritalStatus = "single";
+let country = "Nigeria";
+let age = 45;
+console.log(firstName,lastName,maritalStatus,country,age);
+
+//question9: Declare variables to store your first name, last name, marital status, country and age in a single line
+let firstName2 = "Onyinye", lastName2 = "Vivian", maritalStatus2 = "single", country2 = "Nigeria", age2 = 45;
+console.log(firstName2,lastName2,maritalStatus2,country2,age2);
+
+//question 10: Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+let myAge = 40;
+let yourAge = 70;
+console.log("I am " + myAge + " years old.");
+console.log("You are " + yourAge + " years old.");
